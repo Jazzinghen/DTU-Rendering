079d3fe42df7159652334ad32c44916264036b50
Michele Bianchi <jazzinghen@gmail.com>
2011-12-19 16:43:33 +0100

    Week 03 completed

---
+++ Reports/Week03/Glossy.cpp
@@ -0,0 +1,67 @@
+// 02562 Rendering Framework
+// Written by Jeppe Revall Frisvad, 2011
+// Copyright (c) DTU Informatics 2011
+
+#include <optix_world.h>
+#include "HitInfo.h"
+#include "int_pow.h"
+#include "Glossy.h"
+
+using namespace optix;
+
+#ifndef M_1_PIf
+#define M_1_PIf 0.31830988618379067154
+#endif
+
+float3 Glossy::shade(const Ray& r, HitInfo& hit, bool emit) const
+{
+  // Implement glossy reflection here.
+  //
+  // Input:  r          (the ray that hit the material)
+  //         hit        (info about the ray-surface intersection)
+  //         emit       (passed on to Emission::shade)
+  //
+  // Return: radiance reflected to where the ray was coming from
+  //
+  // Relevant data fields that are available (see Mirror.h and HitInfo.h):
+  // max_depth          (maximum trace depth)
+  // tracer             (pointer to ray tracer)
+  // hit.trace_depth    (number of surface interactions previously suffered by the ray)
+  //
+  // Hint: Use the function shade_new_ray(...) to pass a newly traced ray to
+  //       the shader for the surface it hit.
+	float3 result = make_float3(0.0);
+
+  HitInfo reflected_hit ;
+  Ray reflected_ray;
+  reflected_hit.has_hit = false;
+  HitInfo refracted_hit;
+  refracted_hit.has_hit = false;
+  Ray refracted_ray;
+
+  float R = 1.0;
+  if( hit.trace_depth < max_depth ) // 
+  {
+	
+	tracer->trace_reflected(r, hit, reflected_ray, reflected_hit);
+	tracer->trace_refracted(r, hit, refracted_ray, refracted_hit, R);
+  
+	if( reflected_hit.has_hit )
+		result += shade_new_ray(reflected_ray,reflected_hit) * R;
+	else
+		result += tracer->get_background() * R;
+		  
+	  
+	if( refracted_hit.has_hit)
+		result += shade_new_ray(refracted_ray,refracted_hit) * (1-R);
+	else
+		result += tracer->get_background() * (1-R);
+	
+  }
+  else
+  {
+	  return make_float3(1.0,0.0,0.0);
+  }
+
+  return result + Phong::shade(r, hit, emit);
+}
---
+++ Reports/Week03/RayTracer.cpp
@@ -0,0 +1,85 @@
+// 02562 Rendering Framework
+// Written by Jeppe Revall Frisvad, 2011
+// Copyright (c) DTU Informatics 2011
+
+#include <optix_world.h>
+#include "HitInfo.h"
+#include "ObjMaterial.h"
+#include "fresnel.h"
+#include "RayTracer.h"
+
+using namespace optix;
+
+bool RayTracer::trace_reflected(const Ray& in, const HitInfo& in_hit, Ray& out, HitInfo& out_hit) const
+{
+  // Initialize the reflected ray and trace it. Set out_hit.ray_ior and out_hit.trace_depth.
+  // Return true if the reflected ray hit anything.
+    out_hit.ray_ior = in_hit.ray_ior;
+	out_hit.trace_depth  = in_hit.trace_depth + 1;
+	out = Ray(in_hit.position, optix::reflect(in.direction, in_hit.geometric_normal), 0, 0.001 );
+	
+	trace_to_closest(out,out_hit);
+ 
+  return out_hit.has_hit ;
+}
+
+bool RayTracer::trace_refracted(const Ray& in, const HitInfo& in_hit, Ray& out, HitInfo& out_hit) const
+{
+  // Initialize the refracted ray and trace it. Set out_hit.ray_ior and out_hit.trace_depth.
+  // Return true if the refracted ray hit anything.
+  // Remember that the function must handle total internal reflection.
+
+	float3 refractedDirection;
+	float3 dir;
+    float cosi;
+	float3 normal = in_hit.geometric_normal;
+	float tempIor = get_ior_out(in, in_hit, dir, normal, cosi);
+	bool HasRefraction = optix::refract( refractedDirection, -dir, normal, tempIor/in_hit.ray_ior);
+	out = Ray(in_hit.position, refractedDirection, 0, 0.001 );
+	
+	if ( HasRefraction ){
+		out_hit.trace_depth  = in_hit.trace_depth + 1;
+	    out_hit.ray_ior = tempIor;
+		trace_to_closest(out,out_hit);
+	}
+  return HasRefraction ;  
+}
+
+bool RayTracer::trace_refracted(const Ray& in, const HitInfo& in_hit, Ray& out, HitInfo& out_hit, float& R) const
+{
+  // Initialize the refracted ray and trace it. Set out_hit.ray_ior and out_hit.trace_depth.
+  // Compute the Fresnel reflectance (see fresnel.h) and return it in R.
+  // Return true if the refracted ray hit anything.
+  // Remember that the function must handle total internal reflection.
+  
+  bool res = trace_refracted(in, in_hit, out, out_hit);
+  if(!res)
+  {
+	  R = 1;
+  }
+  else
+  {
+	  float cosi = dot( -in.direction, in_hit.geometric_normal );
+	  float cost = dot( out.direction, -in_hit.geometric_normal );
+
+	  float3 dir;
+	  float3 normal;
+	  R = fresnel_R(cosi, cost, in_hit.ray_ior, out_hit.ray_ior);
+  }
+  return res;
+}
+
+float RayTracer::get_ior_out(const Ray& in, const HitInfo& in_hit, float3& dir, float3& normal, float& cos_theta_in) const
+{
+  normal = in_hit.shading_normal;
+  dir = -in.direction;
+  cos_theta_in = dot(normal, dir);
+	if(cos_theta_in < 0.0)
+	{
+    normal = -normal;
+		cos_theta_in = -cos_theta_in;
+    return 1.0f;
+  }
+  const ObjMaterial* m = in_hit.material;
+  return m ? m->ior : 1.0f;
+}
